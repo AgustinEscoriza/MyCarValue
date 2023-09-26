945212a29cd2be0d7d3a6c2df558017a9ec1489a
AgusEscoriza <AgusEscoriza@outlook.es>
2023-09-26 17:52:25 -0300

    serialization, signin, signup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,24 +6,39 @@ import {   Body,
   Param,
   Delete,
   Query,
-  NotFoundException
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize, SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
-  constructor(private usersService:UsersService){
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService
+  ){
 
   }
   @Post('/signup')
   createUser(@Body() body: CreateUserDto){
-    this.usersService.create(body.email, body.password);
+    this.authService.signup(body.email, body.password);
   }
 
+  @Post('/signin')
+  signin(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password);
+  }
+  
   @Get('/:id')
   async findUser(@Param('id') id: string) {
+
      const user = await this.usersService.findOne(parseInt(id));
      if(!user){
       throw new NotFoundException('user not found')
