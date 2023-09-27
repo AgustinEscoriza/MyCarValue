1bcec6689f398a2385a175f729f760a846add544
AgustinEscoriza <agusescoriza@outlook.es>
2023-09-27 01:06:28 -0300

    agregado cookie session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSesion = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSesion({
+    key: ['asfasfds']
+  }));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true
