0af42d3f43130e795c4db1113e53a09ccdd74e0f
AgusEscoriza <agusescoriza@outlook.es>
2024-06-03 17:40:04 -0300

    new Dtos

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,13 +3,16 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './report.entity';
 import { CreateReportDto } from './dtos/create-report-dto';
+import { User } from 'src/users/user.entity';
 
 Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>){}
 
-  create( reportDTO: CreateReportDto){
-    const report = this.repo.create (reportDTO);
+  create( reportDTO: CreateReportDto, user: User){
+    const report = this.repo.create(reportDTO);
+
+    report.user = user;
 
     return this.repo.save(report);
   }
